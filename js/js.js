15b20317669c716ee2f17ebb79841cf503cbae77
Erik76-81 <126085476+Erik76-81@users.noreply.github.com>
2023-03-03 12:40:05 +0400

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,29 @@
+"use strict"
+
+function i() {
+    korz.style.display = 'block'
+    ver.style.bottom = '15px'
+    ver.style.index = '4'
+    closekorz1.style.display = 'block'
+}
+function ci() {
+    korz.style.display = 'none'
+    ver.style.right = '85px'
+    ver.style.bottom = '70px'
+    closekorz1.style.display = 'none'
+}
+function ob() {
+    burg.style.display = 'block'
+    closeburger.style.display = 'block'
+    // ver.style.bottom = '15px'
+    // ver.style.index = '4'
+}
+function hg() {
+    burg.style.display = 'none'
+    // ver.style.right = '85px'
+    // ver.style.bottom = '70px'
+    closeburger.style.display = 'none'
+}
+
+
+  
